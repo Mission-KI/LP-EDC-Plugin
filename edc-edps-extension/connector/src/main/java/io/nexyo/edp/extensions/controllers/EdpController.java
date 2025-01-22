ff54fa9388f9e486d1a0bd69f6f0d759e9d86416
Oliver Guggenberger <oliver.guggenberger@nexyo.io>
2025-01-22 13:51:33 +0100

    Accept json, return json

--- edc-edps-extension/connector/src/main/java/io/nexyo/edp/extensions/controllers/EdpController.java
+++ edc-edps-extension/connector/src/main/java/io/nexyo/edp/extensions/controllers/EdpController.java
@@ -1,15 +1,27 @@
 package io.nexyo.edp.extensions.controllers;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.edc.runtime.metamodel.annotation.Inject;
+import org.eclipse.edc.spi.monitor.Monitor;
 
 
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Path("/")
 public class EdpController {
 
+    @Inject
+    private Monitor monitor;
+
     @GET
-    @Path("/create-edp")
+    @Path("edp")
     public Response createEdp() {
+        monitor.info("Creating EDP");
 
         return Response.status(Response.Status.OK)
                 .entity("EDP created successfully")
